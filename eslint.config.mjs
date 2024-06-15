153cf4f60960acd62c8fa8de2cdc32f6693c011b
John Canelis <jcanelis@gmail.com>
2024-06-15 00:21:44 -0400

    eslint and updates (#13)
    * eslint and updates
    
    * upgrades ready

---
+++ eslint.config.mjs
@@ -0,0 +1,58 @@
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat"
+import react from "eslint-plugin-react"
+import reactHooks from "eslint-plugin-react-hooks"
+import globals from "globals"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+import js from "@eslint/js"
+import { FlatCompat } from "@eslint/eslintrc"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+export default [
+  {
+    ignores: [
+      "**/dist/",
+      "**/.expo/",
+      "**/node_modules/",
+      "functions/node_modules/",
+      "**/web-build/",
+    ],
+  },
+  ...fixupConfigRules(
+    compat.extends("eslint:recommended", "plugin:react/recommended")
+  ),
+  {
+    plugins: {
+      react: fixupPluginRules(react),
+      "react-hooks": fixupPluginRules(reactHooks),
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+
+      ecmaVersion: 12,
+      sourceType: "module",
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+]
