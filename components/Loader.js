369056fb0fb150ceb35cf5e3f36241048c2e60b6
John Canelis <john@withroam.com>
2024-10-25 00:35:26 -0400

    added animated spinner

--- components/Loader.js
+++ components/Loader.js
@@ -1,10 +1,17 @@
-import React from "react"
-import { View, useWindowDimensions, Text } from "react-native"
+import React, { useEffect } from "react"
+import { Text, useWindowDimensions, View } from "react-native"
 import { useTheme } from "@react-navigation/native"
 
 // Expo
 import { useAssets } from "expo-asset"
-import { Image } from "expo-image"
+
+// React Native Reanimated
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  withRepeat,
+} from "react-native-reanimated"
 
 // Design
 import { baseUnit } from "../constants/Base"
@@ -13,6 +20,26 @@ export default function Loader() {
   const { width } = useWindowDimensions()
   const { colors } = useTheme()
   const [assets, error] = useAssets([require("../assets/loader.png")])
+  const translateX = useSharedValue(0)
+
+  useEffect(() => {
+    translateX.value = withRepeat(
+      withTiming(720, {
+        duration: 4000,
+      }),
+      0,
+      true
+    )
+  }, [translateX])
+
+  // Style object
+  const style = useAnimatedStyle(() => {
+    return {
+      width: 240,
+      height: 240,
+      transform: [{ rotate: `${translateX.value}deg` }],
+    }
+  })
 
   if (error) {
     console.log(error)
@@ -28,14 +55,22 @@ export default function Loader() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        gap: baseUnit * 2,
+        gap: baseUnit * 3,
         backgroundColor: colors.background,
       }}
     >
-      <Image
-        style={{ width: width / 2, height: width / 2 }}
-        source={assets[0].localUri}
-      />
+      <Animated.View
+        style={{
+          width: width / 2,
+          height: width / 2,
+          alignItems: "center",
+          backgroundColor: "pink",
+          justifyContent: "center",
+        }}
+      >
+        <Animated.Image style={style} source={{ uri: assets[0].localUri }} />
+      </Animated.View>
+
       <Text style={{ textAlign: "center", color: colors.text, opacity: 0.7 }}>
         Loading content...
       </Text>
